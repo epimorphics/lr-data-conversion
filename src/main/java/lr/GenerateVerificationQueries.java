6c6dfcf421c8ddf86afc1556fc072bb908a0ea99
Brian McBride <brian@epimorphics.com>
2015-10-15 10:53:28 +0100

    correct name of test query for counting otherPropertyType

--- src/main/java/lr/GenerateVerificationQueries.java
+++ src/main/java/lr/GenerateVerificationQueries.java
@@ -4,7 +4,6 @@ package lr;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
